efb1ad3fef695502a50fdc882f7f5302218a43e6
kostejnv <v.kostejn.vk@gmail.com>
2020-09-08 11:47:03 +0200

    Encode password

--- TCGSync.Entities/User.cs
+++ TCGSync.Entities/User.cs
@@ -37,7 +37,7 @@ namespace TCGSync.Entities
             char[] separator = new char[1] { ';' };
             var dataArray = data.Split(separator);
             TCUsername = dataArray[0];
-            TCPassword = dataArray[1];
+            TCPassword = BasicEncription.Decode(dataArray[1]);
             googleCalendarId = dataArray[2];
             PastSyncInterval = Int32.Parse(dataArray[3]);
             if (dataArray[4] == "false")
@@ -66,7 +66,7 @@ namespace TCGSync.Entities
             StringBuilder data = new StringBuilder();
             data.Append(TCUsername);
             data.Append(";");
-            data.Append(TCPassword);
+            data.Append(BasicEncription.Encode(TCPassword));
             data.Append(";");
             data.Append(googleCalendarId);
             data.Append(";");
@@ -99,5 +99,53 @@ namespace TCGSync.Entities
             Events.Add(event1);
         }
         public override string ToString() => TCUsername;
+
+        private static class BasicEncription
+        {
+            private static readonly int Shift = 5;
+            private static Random random = new Random();
+            public static string Encode(string text)
+            {
+                string encodedText = "";
+                foreach (var character in text)
+                {
+                    encodedText += EncodeChar(character);
+                }
+                return encodedText;
+            }
+            private static string EncodeChar(char character)
+            {
+                char encodeChar = (char)(character + Shift);
+                if (encodeChar == ';' || encodeChar == ',' || encodeChar == '|') encodeChar++;
+                return new string(new char[3] { encodeChar, GetRandomCharacter(), GetRandomCharacter() });
+
+            }
+            private static char GetRandomCharacter()
+            {
+                lock (random)
+                {
+                    int randomChar = random.Next(33, 126);
+                    if (randomChar == ';' || randomChar == ',' || randomChar == '|') randomChar++;
+                    return (char)randomChar;
+                }
+            }
+            public static string Decode(string encodedText)
+            {
+                if (encodedText.Length % 3 != 0) throw new ArgumentException("It do not decode");
+                string decodedText = "";
+                for (int i = 0; i < encodedText.Length/3; i++)
+                {
+                    decodedText += DecodeChar(encodedText.Substring(i * 3, 3));
+                }
+                return decodedText;
+            }
+            private static char DecodeChar(string character)
+            {
+                char decodedChar = character[0];
+                if (decodedChar - 1 == ';' || decodedChar - 1 == ',' || decodedChar - 1 == '|') decodedChar--; 
+                return (char)(decodedChar - Shift);
+
+            } 
+        }
     }
 }
